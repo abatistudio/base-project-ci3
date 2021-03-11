636e0352d4beb52c674ab6d7fd8f4a24c3bf31c6
AbatiStudio <60279584+abatistudio@users.noreply.github.com>
2021-03-11 15:27:35 +0700

    Update Welcome.php

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -3,36 +3,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends TS_Controller {
 
+	public function __construct()
+  {
+    parent::__construct();
+    if($this->session->userdata('user'))
+    {
+      redirect('back/');
+    }
+    $this->tsdata['kelas'] = 'welcome';
+    $this->tsdata['folder']= 'welcome/';
+    $this->tsdata['template'] = 'template/';
+  }
+
 	public function index()
 	{
-		echo '<pre>';
-		print_r($this->tsdata);
+		// echo 'halo';
+		$view = $this->tsdata['folder'].'welcome'; 
+		$this->load->view($view, $this->tsdata);
 	}
 
 	public function userin()
 	{
-		// test fungsi login()
-		$username = 'admin';
-		$password = '123';
-		$result = $this->login($username, $password);
-		if($result['success']){
-			$user = $result['rows'][0];
-			$this->session->set_userdata('user',$user);
+		if(isset($_POST['login']))
+		{
+			$login = html_escape($this->input->post('username'), ENT_QUOTES);
+	    $passw = html_escape($this->input->post('password'), ENT_QUOTES);
+	    $param = array(
+	      'table'=>'users',
+	      'field'=>'*',
+	      'where'=>array('login' => $login, 'password' => mysqlPassword($passw)),
+	    );
+	    $result = $this->tscrud->get($param);
 
-			echo '<pre>';
-			print_r($user);
-		}else{
-			echo '<pre>';
-			print_r($result);
+			if($result['success']){
+				$user = $result['rows'][0];
+				$this->session->set_userdata('user',$user);
+				redirect('back/');
+			}else{
+				$this->session->set_flashdata('login', FALSE);
+			}
 		}
-	}
-
-	public function userout()
-	{
-		$this->logout();
+		$view = $this->tsdata['folder'].'userin'; 
+		$this->load->view($view, $this->tsdata);
 	}
 
 }
 
 /* End of file Welcome.php */
-/* Location: .//home/tamrin/www/baserevoci3/app/controllers/Welcome.php */
+/* Location: ./app/controllers/Welcome.php */
